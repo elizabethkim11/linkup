938a06abe2a6c24c7b7ad1fe40aa82b121283967
ivan-sit <ivansit1214@g.ucla.edu>
2022-11-02 13:08:49 -0700

    set up react-native env

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Text } from "react-native";
+// import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+// import styles from "./assets/styles";
+
+const App = () =>{
+  return <Text>HELLO</Text>
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+export default App;
+
+
+//fdsefgerd
