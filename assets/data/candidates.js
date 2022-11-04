f3958dd74a1b03d32025729492e46502ad2140e9
elizabeth <elizabethkim03@gmail.com>
2022-11-04 15:22:49 -0700

    cat photos!!!

--- assets/data/candidates.js
+++ assets/data/candidates.js
@@ -1,37 +1,37 @@
-// export default [
-//     {
-//       candidateID: '1',
-//       name: 'Elizabeth Kim',
-//       headshot:
-//         'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
-//       blurb: '',
-//     },
-//     {
-//       candidateID: '2',
-//       name: 'Justin Sun',
-//       headshot:
-//         'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
-//       blurb: '',
-//     },
-//     {
-//       candidateID: '3',
-//       name: 'Mithil Chakraborty',
-//       headshot:
-//         'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
-//       blurb: '',
-//     },
-//     {
-//       candidateID: '4',
-//       name: 'Ivan Sit',
-//       headshot:
-//         'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
-//       blurb: '',
-//     },
-//     {
-//       candidateID: '5',
-//       name: 'Chen Tzen',
-//       headshot:
-//         'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
-//       blurb: '',
-//     },
-// ];
+export default [
+    {
+      candidateID: '1',
+      name: 'Elizabeth Kim',
+      headshot:
+        'https://static.boredpanda.com/blog/wp-content/uploads/2019/07/1-5d2cd2a0ac058__880.jpg',
+      blurb: '',
+    },
+    {
+      candidateID: '2',
+      name: 'Justin Sun',
+      headshot:
+        'https://static.boredpanda.com/blog/wp-content/uploads/2019/07/1-5d2cd2a0ac058__880.jpg',
+      blurb: '',
+    },
+    {
+      candidateID: '3',
+      name: 'Mithil Chakraborty',
+      headshot:
+        'https://static.boredpanda.com/blog/wp-content/uploads/2019/07/1-5d2cd2a0ac058__880.jpg',
+      blurb: '',
+    },
+    {
+      candidateID: '4',
+      name: 'Ivan Sit',
+      headshot:
+        'https://static.boredpanda.com/blog/wp-content/uploads/2019/07/1-5d2cd2a0ac058__880.jpg',
+      blurb: '',
+    },
+    {
+      candidateID: '5',
+      name: 'Chen Tzen',
+      headshot:
+        'https://static.boredpanda.com/blog/wp-content/uploads/2019/07/1-5d2cd2a0ac058__880.jpg',
+      blurb: '',
+    },
+];
