21716c771f365751b83b3d66da097fd0ee439d16
elizabeth <elizabethkim03@gmail.com>
2022-11-04 15:10:43 -0700

    create profile display

---
+++ assets/data/candidates.js
@@ -0,0 +1,37 @@
+// export default [
+//     {
+//       candidateID: '1',
+//       name: 'Elizabeth Kim',
+//       headshot:
+//         'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
+//       blurb: '',
+//     },
+//     {
+//       candidateID: '2',
+//       name: 'Justin Sun',
+//       headshot:
+//         'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
+//       blurb: '',
+//     },
+//     {
+//       candidateID: '3',
+//       name: 'Mithil Chakraborty',
+//       headshot:
+//         'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
+//       blurb: '',
+//     },
+//     {
+//       candidateID: '4',
+//       name: 'Ivan Sit',
+//       headshot:
+//         'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
+//       blurb: '',
+//     },
+//     {
+//       candidateID: '5',
+//       name: 'Chen Tzen',
+//       headshot:
+//         'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
+//       blurb: '',
+//     },
+// ];
