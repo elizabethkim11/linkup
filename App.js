55bd55ac646d144d41661567febeafa5632bf8a0
Justin Sun <justin@Justins-MacBook-Pro-2.local>
2022-11-08 12:00:51 -0800

    Implemented basic button feature

--- App.js
+++ App.js
@@ -3,17 +3,30 @@ import React from 'react';
 import {Text, Image, View, StyleSheet, ImageBackground} from 'react-native';
 import Profile from './linkup-frontend/src/components/profile/index.js';
 import users from './assets/data/candidates'
-
-const meow = {
-  name: 'sir meowington',
-  blurb: 'cute kitty',
-  headshot: 'https://static.boredpanda.com/blog/wp-content/uploads/2019/07/1-5d2cd2a0ac058__880.jpg',
-}
+import Animated, { useSharedValue, useAnimatedStyle} from 'react-native-reanimated'
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
 const App = () =>{
+
+  const sharedValue = useSharedValue(1);
+
+  const profileStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: sharedValue.value * 500 - 250,
+      },
+    ],
+  }));
+
   return (
     <View style={styles.pageContainer}>
+      <Animated.View style = {[styles.animatedCard, profileStyle]}>
       <Profile user={users[0]} />
+      </Animated.View>
+      <Pressable onPress={() => (sharedValue.value = Math.random())}
+        style={{position: 'absolute', top: 250, zIndex: 100}}>
+        <Text>Meow</Text>
+        </Pressable>
     </View>
   );
 }; 
@@ -24,6 +37,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  animatedCard: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
 
 export default App;
