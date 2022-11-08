547c00967df714f987b238eb0cb483bc47f9d45a
elizabeth <elizabethkim03@gmail.com>
2022-11-08 10:43:49 -0800

    Initial profile card finished

--- App.js
+++ App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import {Text, Image, View, StyleSheet, ImageBackground} from 'react-native';
-// import { createBottomTabNavigator, createAppContainer } from "react-navigation";
-// import styles from "./assets/styles";
-// import Profile from './src/components/index.js';
-// import candidates from './assets/data/candidates.js';
 import Profile from './linkup-frontend/src/components/profile/index.js';
+import users from './assets/data/candidates'
 
 const meow = {
   name: 'sir meowington',
@@ -15,7 +12,7 @@ const meow = {
 const App = () =>{
   return (
     <View style={styles.pageContainer}>
-      <Profile user={meow} />
+      <Profile user={users[4]} />
     </View>
   );
 }; 
@@ -23,10 +20,9 @@ const App = () =>{
 const styles = StyleSheet.create({
   pageContainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
 });
 
 export default App;
