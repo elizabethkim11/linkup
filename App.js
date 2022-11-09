2f7133e65fc43e09c2d64e234053a13038caa8a6
Justin Sun <justin@Justins-MacBook-Pro-2.local>
2022-11-08 18:03:34 -0800

    improved visuals of swiping via rotation

--- App.js
+++ App.js
@@ -5,28 +5,36 @@ import Profile from './linkup-frontend/src/components/profile/index.js';
 import users from './assets/data/candidates'
 import Animated, { useSharedValue, 
   useAnimatedStyle, 
-  withSpring,
-  useAnimatedGestureHandler
+  useDerivedValue,
+  useAnimatedGestureHandler,
+  interpolate
 } from 'react-native-reanimated'
 import {PanGestureHandler} from 'react-native-gesture-handler'
-const App = () =>{
-
+import useWindowDimensions from 'react-native/Libraries/Utilities/useWindowDimensions';
+const App = () =>{ 
+  const {width: screenWidth} =  useWindowDimensions();
   const translateX = useSharedValue(0);
+  const rotate = useDerivedValue( () => 
+    interpolate(translateX.value, [0, screenWidth * 2], [0,60]) + 'deg',
+  );
 
   const profileStyle = useAnimatedStyle(() => ({
     transform: [
       {
         translateX: translateX.value
       },
+      {
+        rotate: rotate.value
+      },
     ],
   }));
 //what to do when user drags and releases items
   const gestureHandler = useAnimatedGestureHandler({
-    onStart: (_,context) => {
-      console.log('start')
+    onStart: (_, context) => {
+      context.startPos=translateX.value
     },
-    onActive: (event) => {
-      translateX.value = event.translationX;
+    onActive: (event, context) => {
+      translateX.value = context.startPos + event.translationX;
     },
     onEnd: () => {
       console.log('done')
