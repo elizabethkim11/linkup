28da45ee68c96e68b504558884e0fa7a5c4c18ea
elizabeth <elizabethkim03@gmail.com>
2022-11-26 01:19:16 -0500

    download icons

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import Home from 'linkup/linkup-frontend/src/screens/Home.js'
-import Notifs from 'linkup/linkup-frontend/src/screens/Notifs.js'
+import {View, StyleSheet, Text, SafeAreaView, Pressable} from 'react-native';
+import Home from 'linkup/linkup-frontend/src/screens/Home.js';
+import Notifs from 'linkup/linkup-frontend/src/screens/Notifs.js';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+// import MaterialCommunityIcons from 'react-native-vector-icons/MateralCommunityIcons';
 
 const App = () => { 
- 
+  const color = "b5b5b5"
+  const pressedColor = "#F76C6B"
   return ( 
-    <View style={styles.pageContainer}> 
-      <Notifs />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.pageContainer}> 
+        <View style={styles.topNavigation}>
+          <Pressable>
+            <FontAwesome name="user" size={30} color={color} />
+          </Pressable>
+          <FontAwesome name="user" size={30} color={color} />
+          {/* <Icon name="ios-chatbubbles" size={30} color={color} /> */}
+        </View>
+        <Home/>
+      </View>
+    </SafeAreaView>
   );
 }; 
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   pageContainer: {
     flex: 1,
     alignItems: 'center',
