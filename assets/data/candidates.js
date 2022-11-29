fb89f84fdd019395c37b28c58f0908113da016c1
elizabeth <elizabethkim03@gmail.com>
2022-11-29 14:41:06 -0800

    update file names

--- assets/data/candidates.js
+++ assets/data/candidates.js
@@ -4,14 +4,20 @@ export default [
       name: 'Elizabeth Kim',
       headshot:
         'https://media-exp1.licdn.com/dms/image/D5603AQGChx14p66b9Q/profile-displayphoto-shrink_400_400/0/1668126608019?e=1673481600&v=beta&t=_JtJ3Ub_2lcXBsKbdZgr1Vn1JnQ1F-ienOOlZcpatUU',
-      blurb: 'CS+Ling Squad',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
     },
     {
       candidateID: '2',
       name: 'Justin Sun',
       headshot:
         'https://media-exp1.licdn.com/dms/image/C5603AQHV4B6uD-gQiA/profile-displayphoto-shrink_800_800/0/1662439871912?e=1673481600&v=beta&t=K52hJxvr3maSb_WmD3LEB4Zoq5fAqCDZ3Kp0eeJamHs',
-      blurb: '',
+      blurb: 'I love CS and cats!',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
     },
     {
       candidateID: '3',
@@ -19,6 +25,9 @@ export default [
       headshot:
         'https://media-exp1.licdn.com/dms/image/D5603AQG00Hyk_xV1gA/profile-displayphoto-shrink_400_400/0/1668673226672?e=1674691200&v=beta&t=c05ju0JtSTS0NcXx8AHAP6LtxUTcrelOWr6mFcc6rAw',
       blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
     },
     {
       candidateID: '4',
@@ -26,6 +35,9 @@ export default [
       headshot:
         'https://media-exp1.licdn.com/dms/image/C5603AQE3zF28fIpzGQ/profile-displayphoto-shrink_800_800/0/1654468565991?e=1673481600&v=beta&t=T71kMY57uN0_r3LITSpJymqPS_aqfoJvSeUK-_qrwbE',
       blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
     },
     {
       candidateID: '5',
@@ -33,5 +45,108 @@ export default [
       headshot:
         'https://media-exp1.licdn.com/dms/image/C4E03AQHpV4GYuyxQRQ/profile-displayphoto-shrink_800_800/0/1637965242730?e=1673481600&v=beta&t=1D16AQVEorZCOuFsYCpIJHyjHzbPqaGdDBwGV7MTmdI',
       blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
+    },
+    {
+      candidateID: '6',
+      name: 'Ved Phadke',
+      headshot:
+        'https://media-exp1.licdn.com/dms/image/C5603AQGBxCgns0ifVQ/profile-displayphoto-shrink_800_800/0/1648914568728?e=1675296000&v=beta&t=dM8dTSo8V-FY58-VeiMD4tptMVXR7cBNNEDvBeWRwYs',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
+    },
+    {
+      candidateID: '7',
+      name: 'Chen Tzen',
+      headshot:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHpV4GYuyxQRQ/profile-displayphoto-shrink_800_800/0/1637965242730?e=1673481600&v=beta&t=1D16AQVEorZCOuFsYCpIJHyjHzbPqaGdDBwGV7MTmdI',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
+    },
+    {
+      candidateID: '8',
+      name: 'Chen Tzen',
+      headshot:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHpV4GYuyxQRQ/profile-displayphoto-shrink_800_800/0/1637965242730?e=1673481600&v=beta&t=1D16AQVEorZCOuFsYCpIJHyjHzbPqaGdDBwGV7MTmdI',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
+    },
+    {
+      candidateID: '9',
+      name: 'Chen Tzen',
+      headshot:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHpV4GYuyxQRQ/profile-displayphoto-shrink_800_800/0/1637965242730?e=1673481600&v=beta&t=1D16AQVEorZCOuFsYCpIJHyjHzbPqaGdDBwGV7MTmdI',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
+    },
+    {
+      candidateID: '10',
+      name: 'Chen Tzen',
+      headshot:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHpV4GYuyxQRQ/profile-displayphoto-shrink_800_800/0/1637965242730?e=1673481600&v=beta&t=1D16AQVEorZCOuFsYCpIJHyjHzbPqaGdDBwGV7MTmdI',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
+    },
+    {
+      candidateID: '11',
+      name: 'Chen Tzen',
+      headshot:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHpV4GYuyxQRQ/profile-displayphoto-shrink_800_800/0/1637965242730?e=1673481600&v=beta&t=1D16AQVEorZCOuFsYCpIJHyjHzbPqaGdDBwGV7MTmdI',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
+    },
+    {
+      candidateID: '12',
+      name: 'Chen Tzen',
+      headshot:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHpV4GYuyxQRQ/profile-displayphoto-shrink_800_800/0/1637965242730?e=1673481600&v=beta&t=1D16AQVEorZCOuFsYCpIJHyjHzbPqaGdDBwGV7MTmdI',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
+    },
+    {
+      candidateID: '13',
+      name: 'Chen Tzen',
+      headshot:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHpV4GYuyxQRQ/profile-displayphoto-shrink_800_800/0/1637965242730?e=1673481600&v=beta&t=1D16AQVEorZCOuFsYCpIJHyjHzbPqaGdDBwGV7MTmdI',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
+    },
+    {
+      candidateID: '14',
+      name: 'Chen Tzen',
+      headshot:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHpV4GYuyxQRQ/profile-displayphoto-shrink_800_800/0/1637965242730?e=1673481600&v=beta&t=1D16AQVEorZCOuFsYCpIJHyjHzbPqaGdDBwGV7MTmdI',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
+    },
+    {
+      candidateID: '15',
+      name: 'Chen Tzen',
+      headshot:
+        'https://media-exp1.licdn.com/dms/image/C4E03AQHpV4GYuyxQRQ/profile-displayphoto-shrink_800_800/0/1637965242730?e=1673481600&v=beta&t=1D16AQVEorZCOuFsYCpIJHyjHzbPqaGdDBwGV7MTmdI',
+      blurb: '',
+      school: 'UCLA',
+      year: '2025',
+      resume: '',
     },
 ];
